f8582d0d44a056a2fca7adaaac82237d40254836
tejas-mn <68629215+tejas-mn@users.noreply.github.com>
2024-06-04 22:46:13 +0530

    Add files via upload

---
+++ test/main2.py
@@ -0,0 +1,17 @@
+# my_module.py
+
+def square(n):
+    """
+    Takes in a number n and returns the square of n.
+
+    Args:
+        n (float or int): The input number.
+
+    Returns:
+        float or int: The square of the input number.
+    """
+    return n**2
+
+# Usage example
+result = square(5)
+print(f"The square of 5 is {result}")
